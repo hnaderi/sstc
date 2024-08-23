9a6091777bbe7b818580b986baf19c8ee9c9cccf
Hossein Naderi <hossein-naderi@hotmail.com>
2024-08-23 13:19:02 +0330

    Added nix and scripts

---
+++ shell.nix
@@ -0,0 +1,10 @@
+let
+  pkgs = import <nixpkgs> { };
+  kicad = pkgs.kicad;
+  compile = pkgs.writeShellScriptBin "compile" ''
+    ${kicad}/bin/kicad-cli sch export $1 sstc.kicad_sch
+  '';
+in pkgs.mkShell {
+  name = "sstc";
+  buildInputs = [ compile kicad ];
+}
