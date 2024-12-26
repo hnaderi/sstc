bfb557d88c30025b083692b52492afb30353d659
Hossein Naderi <hossein-naderi@hotmail.com>
2024-12-26 09:19:19 +0330

    Added videos, webpage uses EJS

--- shell.nix
+++ shell.nix
@@ -8,16 +8,24 @@ let
     ls parts/*.FCStd | xargs -i bash -c "freecad --console {} < parts/export.py"
   '';
   generate = pkgs.writeShellScriptBin "generate" ''
+    echo "Generating schematics ..."
+
     compile pdf sstc-v1/sstc.kicad_sch
     compile svg sstc-v1/sstc.kicad_sch
     compile pdf sstc-v2/sstc-v2.kicad_sch
     compile svg sstc-v2/sstc-v2.kicad_sch
+
+    echo "Generating part objects ..."
     exportSTLs
+
+    echo "Generating website ..."
 
     mkdir -p public;
     mv *.pdf public
     mv *.svg public
     mv parts/*.stl public
+
+    ${pkgs.ejs}/bin/ejs -f webpage/data.json -o public/index.html webpage/index.ejs
   '';
 in pkgs.mkShell {
   name = "sstc";
